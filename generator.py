dd192a6c4dd3a87ab6991ee72ff2db576f35bf92
Mercys-s <dehgsk58@gmail.com>
2024-03-21 21:53:54 +0900

    start

---
+++ generator.py
@@ -0,0 +1,10 @@
+def generator_user():
+    pass
+
+def generator_event(id, event_type , user_id , date_time, **kwargs):
+    pass
+
+# предположительно это **kwargs
+pararms = ['Источник', 'Тип браузера']
+
+metrics
