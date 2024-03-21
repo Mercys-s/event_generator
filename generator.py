a180e857c2e57ed626e313bfa1f0ffe821b0a724
Mercys-s <dehgsk58@gmail.com>
2024-03-21 22:38:46 +0900

    Settings added

--- generator.py
+++ generator.py
@@ -5,6 +5,37 @@ def generator_event(id, event_type , user_id , date_time, **kwargs):
     pass
 
 # предположительно это **kwargs
-pararms = ['Источник', 'Тип браузера']
+kwargs = {
+    'params': {
+        'Источник': 'переменная',
+        'Тип устройства': 'переменная',
+    },
+    'metrics': {
+        'Сумма заказа': 'переменная (int)',
+        'Количество страниц': 'переменная (int)',
+        'Успешно': 'переменная (bool)'
+    }
+}
 
-metrics
+success_metric = ['Успешно']
+settings = {
+    1: {
+        'params': ['Источник', 'Тип браузера',],
+        'metrics': ['Количество страниц',]
+    },
+
+    2: {
+        'params': ['Источник',],
+        'metrics': success_metric
+    },
+
+    3: {
+        'params': None,
+        'metrics': ['Сумма заказа'] + success_metric
+    },
+
+    4: {
+        'params': None,
+        'metrics': ['Сумма заказа'] + success_metric
+    }
+}
